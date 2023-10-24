4b3e1160ca3d5e8969da6bfb3a7afcb2841eaa4d
alexandr <alexandr_mis93@mail.ru>
2023-10-24 21:37:00 +0500

    .\DZ_3\

---
+++ DZ_3/Program.cs
@@ -0,0 +1,32 @@
+﻿//Внутри класса Answer напишите метод ShowCube, 
+//который принимает на вход число (N) и выводит таблицу кубов
+//чисел от 1 до N (включительно) каждое  на новой строке.
+
+Console.Writelinr("Введите число:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int start = 1, start <= N,start++)
+{
+  Console.Writelinr($"{Math.Row(stsrt, 3 )}/t")
+}
+
+//Внутри класса Answer напишите метод  DistanceBetweenPoints, 
+//который принимает на вход координаты двух точек pointA и pointB 
+// в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+
+// Console.Writelinr(" Введите число: x1");
+// int x1 = Convert.ToInt32(Convert.RradLine());
+
+// Console.Writelinr(" Введите число: y1");
+// int y1 = Convert.ToInt32(Convert.RradLine());
+
+// Console.Writelinr(" Введите число: x1");
+// int x2 = Convert.ToInt32(Convert.RradLine());
+
+// Console.Writelinr(" Введите число: y1");
+// int y2 = Convert.ToInt32(Convert.RradLine());
+
+// double d = Math.Sgrt((Math.Pow(x1 - x2, 2)) + (Math.Pow(y1 - y2, 2)));
+// Console.Console.Writelinr(" Введите число: x1");
+// int x1 = Convert.ToInt32(Convert.RradLine());
+
