ef1238a921990ebd4a4feacbd8f9531896dd98ea
alexandr <alexandr_mis93@mail.ru>
2023-10-26 22:40:50 +0500

    .\DZ_3\

--- DZ_3/Program.cs
+++ DZ_3/Program.cs
@@ -17,28 +17,50 @@
 //который принимает на вход координаты двух точек pointA и pointB 
 // в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
 
-int[] pointA;
- int[] pointB;
-Console.Write(" Введите число x1:  ");
-int x1 = Convert.ToInt32(Console.ReadLine());
+// using System.Globalization;
 
-Console.Write(" Введите число y1:  ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+// int[] pointA;
+//  int[] pointB;
+// Console.Write(" Введите число x1:  ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" Введите число z1:  ");
-int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число y1:  ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" Введите число x1:  ");
-int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число z1:  ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(" Введите число y1 :  ");
-int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число x1:  ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(" Введите число y1 :  ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
       
-Console.Write(" Введите число z1 :  ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите число z1 :  ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double d = Math.Sqrt((Math.Pow(x1 - x2, 2)) + (Math.Pow(y1 - y2, 2)) + (Math.Pow(z2 - z1, 2)));
+// double d = Math.Sqrt((Math.Pow(x1 - x2, 2)) + (Math.Pow(y1 - y2, 2)) + (Math.Pow(z2 - z1, 2)));
 
-Console.WriteLine($"d = {Math.Round(d , 2)}");
+// Console.WriteLine($"d = {Math.Round(d , 2)}");
+
+
+// // Внутри класса Answer напишите метод IsPalindrome, 
+// который принимает на вход пятизначное число number и проверяет,
+//  является ли оно палиндромом.
+// // Метод должен проверить является ли число пятизначным, 
+// в противном случае - вывести Число не пятизначное и False в следующей строке.
+// // Для остальных чисел вернуть True или False.
+
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number < 10000 || number > 99999)
+{
+    Console.WriteLine("Число не пятизначное ");
+    Console.WriteLine("False");
+}
+else 
+Console.WriteLine(number);
+
 
 
