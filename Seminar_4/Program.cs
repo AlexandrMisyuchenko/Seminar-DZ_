7fb96820105514ae79d9466e6ee851267b91bf52
alexandr <alexandr_mis93@mail.ru>
2023-10-26 23:43:03 +0500

    .\Seminar_4\

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+// !!!! Методы !!!!
+
+// Методы можно повторно использрвать, просто поменяв данные;
+
+// Метолы: которые возвращают значения и voitd
+// тип_метоад ИмяМетода(параметр1, параметр2)
+// Параметра: два числа и знак арифм. операции
+
+int Calculate(int first, int second, char sign)
+{
+    if (sign == '+') // '' - это только для char
+    {
+        return first + second;
+    }
+    
+     else if (sign == '-') // '' - это только для char
+    {
+        return first - second;
+    }
+     else if (sign == '*') // '' - это только для char
+    {
+        return first * second;
+    }
+    else if (sign == '/') // '' - это только для char
+    {
+        return first / second;
+    }
+   else // Другие "знаки": всеБ кроме "+", "-", "*";
+   {
+    Console.WriteLine("Ошибка при вводе знака");
+    return 0;
+   }
+
+}
+
+//Calculate(int first, int second, char sign)
+// Используем метод - вызов
+Console.WriteLine(Calculate(100, 5, '+'));
+// first=2, second=5, char= +;
+Console.WriteLine(Calculate(100, 35, '-'));
+// first=2, second=5, char= +;
+Console.WriteLine(Calculate(100, 35, '*'));
+// first=2, second=5, char= +;
+Console.WriteLine(Calculate(100, 25, '/'));
+// first=2, second=5, char= +;
+
+
+
+остановился на 49 минуте
