20faa8a3202cc5c185097294edcffda5fbd686df
alexandr <alexandr_mis93@mail.ru>
2023-11-04 18:37:59 +0500

    .\DZ_3\

--- DZ_3/Program.cs
+++ DZ_3/Program.cs
@@ -44,6 +44,36 @@
 // Console.WriteLine($"d = {Math.Round(d , 2)}");
 
 
+
+/ Внутри класса Answer напишите метод DistanceBetweenPoints, 
+// который принимает на вход координаты двух точек pointA и
+//  pointB в виде массива целых чисел, 
+//  и возвращает расстояние между ними в 3D пространстве.
+
+//  A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Введите значение координаты x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение координаты y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение координаты z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение координаты x1: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение координаты y1: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение ккоординаты z1: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 -y2, 2) + Math.Pow(z1 - z2, 2));
+// Console.WriteLine($" d = {Math.Round(d , 2)}");
+
 // // Внутри класса Answer напишите метод IsPalindrome, 
 // который принимает на вход пятизначное число number и проверяет,
 //  является ли оно палиндромом.
