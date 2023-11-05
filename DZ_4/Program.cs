5196d5aa2373320fa95008413db899f0134eb65b
alexandr <alexandr_mis93@mail.ru>
2023-11-05 23:14:43 +0500

    .\DZ_4\

--- DZ_4/Program.cs
+++ DZ_4/Program.cs
@@ -4,6 +4,7 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+//  Вариант №1
 // Console.Write("Введите число А: ");
 // int numberA = Convert.ToInt32(Console.ReadLine()); 
 
@@ -12,11 +13,26 @@
 // Console.WriteLine($"{numberA}, {numberB} => {Math.Pow(numberA, numberB )}");
 
 
+//  Вариант №2
+// Console.Write("Введите число А: ");
+// int numberA = Convert.ToInt32(Console.ReadLine()); 
 
-Console.Write("Введите число А: ");
-int numberA = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Введите число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write($" Число {numberA} в степение {numberB} => {GetBegree(numberA, numberB)}");
+// int GetBegree (int number, int Begree) // Get - получить, Begree - степень
+// {
+//     int sumBegree = 1;
+//     for (int i = 0; i < Begree; i++ )
+//     {
+//         sumBegree *= number;
+//     }
+//     return sumBegree;
+// }
 
-Console.Write("Введите число B: ");
-int numberB = Convert.ToInt32(Console.ReadLine()); 
 
-int exponentiation ()
+// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
