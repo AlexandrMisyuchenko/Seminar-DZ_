db2abb10da6c0f5ef42a7adde2bf2f57d9b809a1
alexandr <alexandr_mis93@mail.ru>
2023-11-05 18:15:15 +0500

    .\DZ_4\

---
+++ DZ_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на
+// вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.Write("Введите число А: ");
+// int numberA = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.Write("Введите число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine($"{numberA}, {numberB} => {Math.Pow(numberA, numberB )}");
+
+
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine()); 
+
+int exponentiation ()
