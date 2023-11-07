50382a9023e8c5c48184b000e82f7e3005975595
alexandr <alexandr_mis93@mail.ru>
2023-11-07 17:31:19 +0500

    .\DZ_4\

--- DZ_4/Program.cs
+++ DZ_4/Program.cs
@@ -55,3 +55,20 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// int[] result = GetBinaryArray (8);// Получили массив на 8 элементов
+// Console.WriteLine($"[{String.Join("; ", result)}]");
+// int[] GetBinaryArray (int size)
+// {
+//     // тип данных [] имя_масива  = new int[размер]
+//     int[] array = new int [size];
+//     // Изначально массив заполнен НУЛЯМИ
+//     for ( int i = 0; i < array.Length; i++)
+//     // array.Length = size
+//     {
+//         array[i] = new Random().Next(7, 131);
+//         // кладём в каждую переменную [i] рандомное число
+//         // Next(2) => Next(0, 2)
+//     }
+//     return array;
+// }
